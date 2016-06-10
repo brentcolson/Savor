78dff6202219512cfbd1853eeb87dc79fa22ebae
Brent Colson <bacolson@gmail.com>
2016-06-10 17:28:14 -0400

    Updated the put method handler and controller for updating a restaurant

--- server/handlers/handlers.js
+++ server/handlers/handlers.js
@@ -27,12 +27,13 @@ module.exports = {
     var restaurant = req.body;
     restaurantController.addRestaurantReview(restaurant, function(newRestaurant) {
       res.status(201).json(newRestaurant);
-    })
+    });
   },
+  
   //function not being used
   updateRestaurantInfo: function(req, res) {
     //_.id
-    var id = req.params._id;
+    var id = req.params.id;
     var newProperties = req.body;
     restaurantController.updateOne(id, newProperties, function(updatedRestaurant) {
       res.status(200).json(updatedRestaurant);
